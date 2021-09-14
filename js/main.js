ff68989ed23f3bbe2d8583694cb8ee6b503ff0b2
Amarildo <amarildop1dev@gmail.com>
2021-09-14 20:17:40 -0300

    Organizando a tabela de jogos e adicionado arquivo separado com os jogos

--- js/main.js
+++ js/main.js
@@ -1,6 +1,72 @@
 import equipes from "./model/dataSetEquipes.js";
 import menu from "./model/menu.js";
 import rodape from "./model/rodape.js";
+import jogos from "./model/dataSetJogos.js";
+
+function insereJogos(){
+    for(const jogo of jogos){
+        addJogosNaTabela(jogo);
+    }
+}
+
+    /*         <div class="equipe">
+            <img src="${time.imagem}">
+            <ul class="listaEquipe">
+                <li><span>Técnico: </span>${time.tecnico}</li>
+                <li><span>Fundação: </span>${time.fundacao}</li>
+                <li><span>Ginásio: </span>${time.ginasio}</li>
+                <li><span>Endereço: </span>${time.endereco}</li>
+            </ul>
+        </div> */
+
+        /*         idjogo: '20',
+        data: '01/12/2021',
+        hora: '08:00',
+        equipeA: 'Jampa',
+        placar: 'x',
+        equipeB: 'Campina',
+        local: 'Vila Olímpica', */
+
+
+function addJogosNaTabela(game){
+
+    /* JOGOS PARA A TELA DO DESKTOP */
+    const jogoTelaDesktop = `
+                <tr>
+                    <td> ${game.idjogo} </td>
+                    <td> <br>${game.data}</br> ${game.dia} </td>
+                    <td> ${game.hora} </td>
+                    <td> ${game.equipeA} </td>
+                    <td> ${game.placar} </td>
+                    <td> ${game.equipeB} </td>
+                    <td><br> ${game.local} <br> ${game.bairro} </td>
+                </tr>
+    `;
+    const mostraJogoDesktop = document.querySelector('.telaDesktop');
+    mostraJogoDesktop.insertAdjacentHTML('beforeend', jogoTelaDesktop);
+
+    /* JOGOS PARA A TELA DE SMARTPHONES */
+    const jogoTelaMobile = `
+                        <tr>
+                            <td> ${game.idjogo} </td>
+                            <td> <br>${game.data}</br>
+                                <br>${game.dia}</br>
+                                <br>${game.hora}</br>
+                            </td>
+                            <td> <br>${game.equipeA} </br>x<br> ${game.equipeB} </br> </td>
+                            <td><br> ${game.local},<br> ${game.bairro} </td>
+                        </tr>      
+    `;
+    const mostraJogoMobile = document.querySelector('.telaMobile');
+    mostraJogoMobile.insertAdjacentHTML('beforeend', jogoTelaMobile);
+
+}
+
+insereJogos();
+
+
+
+
 
 /* INSERINDO MENU NAS PÁGINAS */
 function insereMenu(){
@@ -17,6 +83,8 @@ function addItemMenu(itemMenu){
     mostraItemMenu.insertAdjacentHTML('beforeend', item);
 }
 insereMenu();
+
+
 
 /* INSERINDO RODAPÉ NAS PÁGINAS */
 function insereItemRodape(){
---
+++ js/model/dataSetJogos.js
@@ -0,0 +1,38 @@
+const jogos = [  
+
+    {
+        idjogo: '21',
+        data: '28/11/2021',
+        dia: 'Sexta',
+        hora: '16:00',
+        equipeA: 'Jp',
+        placar: 'x',
+        equipeB: 'Cg',
+        local: 'Vila Olímpica',
+        bairro: 'Bairro dos Estados',
+    },
+    {
+        idjogo: '20',
+        data: '01/12/2021',
+        dia: 'Domingo',
+        hora: '08:00',
+        equipeA: 'Jampa',
+        placar: 'x',
+        equipeB: 'Campina',
+        local: 'Vila Olímpica',
+        bairro: 'Bairro dos Estados',
+    },
+    {
+        idjogo: '19',
+        data: '05/12/2021',
+        dia: 'Sábado',
+        hora: '10:00',
+        equipeA: 'Facisa',
+        placar: 'x',
+        equipeB: 'Botafogo',
+        local: 'Vila Olímpica',
+        bairro: 'Bairro dos Estados',
+    },
+];
+
+export default jogos;
